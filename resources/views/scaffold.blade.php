b5734e17b94ec29f912a7b9c28665bbac9adcf65
Adamlin0708 <kuso12610@gmail.com>
2017-10-20 01:47:43 +0800

    建立scaffold

--- resources/views/scaffold.blade.php
+++ resources/views/scaffold.blade.php
@@ -5,25 +5,43 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/web.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <nav class="navbar">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <img src="img/Aysuveda_logo.jpg">
+    <div class="container-fluid modify-nav">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{route('home')}}"><img src="img/Aysuveda_logo.jpg">
+                    </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#" class="test">關於阿蘇</a></li>
+                        <li><a href="#">調和導師</a></li>
+                        <li><a href="#">個別調和課程</a></li>
+                        <li><a href="#">團體調和課程</a></li>
+                        <li><a href="#">調和行旅</a></li>
+                        <li><a href="#">商店</a></li>
+                        <li><a href="#">臉書專頁</a></li>
+                    </ul>
+                </div>
+                <!--/.nav-collapse -->
             </div>
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-                <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
-            </ul>
-        </div>
-    </nav>
+            <!--/.container-fluid -->
+        </nav>
+    </div>
     @yield('content')
     @yield('footer')
     @yield('javascript')
 </body>
 </html>
+
