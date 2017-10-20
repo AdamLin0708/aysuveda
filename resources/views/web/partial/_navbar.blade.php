bdfd2b67da2868166c733d863e17883143a08e35
Adamlin0708 <kuso12610@gmail.com>
2017-10-20 18:26:32 +0800

    navbar初版

---
+++ resources/views/web/partial/_navbar.blade.php
@@ -0,0 +1,65 @@
+<div class="container-fluid modify-nav">
+    <nav class="navbar ">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{route('home')}}"><img src="/img/Aysuveda_logo.jpg">
+                </a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{route('about')}}">關於阿蘇</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">調和導師</a>
+                        <ul class="dropdown-menu dropdown-menu-left">
+                            <li><a href="{{route('team.ceo')}}">創意總監</a></li>
+                            <li><a href="{{route('team.educationDirector')}}">教育總監</a></li>
+                            <li><a href="#">行政總監</a></li>
+                            <li><a href="#">調和導師團隊</a></li>
+                            <li><a href="#">全腦運動教練</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">個別調和課程</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">團體調和課程</a>
+                        <ul class="dropdown-menu dropdown-menu-left">
+                            <li><a href="#">BrainGym®丹尼遜健腦操</a></li>
+                            <li><a href="#">Touch For Health~觸康健®</a></li>
+                            <li><a href="#">CSF個人形象</a></li>
+                            <li><a href="#">RayKey色彩香氣</a></li>
+                            <li><a href="#">Reiki靈氣</a></li>
+                            <li><a href="#">NGH催眠</a></li>
+                            <li><a href="#">Ayurveda阿育吠陀</a></li>
+                            <li><a href="#">Feldenkrais費登奎斯</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">調和行旅</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">商店</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li><a href="#">能量寶石</a></li>
+                            <li><a href="#">色彩香氣</a></li>
+                            <li><a href="#">肌動學</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">臉書</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li><a href="#">丹尼遜健腦操</a></li>
+                            <li><a href="#">調和的剛剛好</a></li>
+                            <li><a href="#">時尚療癒空間</a></li>
+                            <li><a href="#">line親子調和群組</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+        <!--/.container-fluid -->
+    </nav>
+</div>
