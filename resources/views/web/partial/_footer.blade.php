55333172cd06633d89f7f1eaac68e4149702828e
Adamlin0708 <kuso12610@gmail.com>
2017-10-20 18:26:06 +0800

    footer 初版

---
+++ resources/views/web/partial/_footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="footer">
+    <div class="container-fluid page-container-responsive">
+        <div class="row">
+            <div class="footer__links col-md-4">
+                <label style="font-size: x-large" >服務內容</label>
+                <ul class="list-group">
+                    <li style="list-style: none">
+                        <a class="macho-4 footer-link" href="#">- 預約居家照護</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__links col-md-5">
+                <label style="font-size: x-large">聯繫我們</label>
+                <ul class="list-group" style="font-size: medium">
+                    {{--<li style="list-style: none">--}}
+                    {{--<p class="macho-4">(02)2363-5100</p>--}}
+                    {{--</li>--}}
+                    <li style="list-style: none">
+                        <p class="macho-4">TEL: (02)2363-5100 /  0800-800-069</p>
+                    </li>
+                    <li style="list-style: none">
+                        <p class="macho-4">FAX: (02)2363-1738</p>
+                    </li>
+                    <li style="list-style: none">
+                        <p class="macho-4">info@ucarer.com.tw</p>
+                    </li>
+                    <li style="list-style: none">
+                        <p class="macho-4">106台北市大安區羅斯福路三段149號3樓之1</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__links col-md-3">
+                <label style="font-size: x-large" >相關連結</label>
+                <ul class="list-group">
+                    <li style="list-style: none">
+                        <a class="macho-4 footer-link " href="https://www.facebook.com/ucarertw/?ref=bookmarks" target="_blank">- 優照護粉絲專頁</優照護粉絲專頁></a>
+                    <li style="list-style: none">
+                        <a class="macho-4 footer-link " href="https://www.youtube.com/channel/UCD4sKZLscdzUOs7XhTKsbFA" target="_blank">- 優照護Youtube</a>
+                    </li>
+                </ul>
+                <label style="font-size: x-large">服務地區</label>
+                <p>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="footer__links col-md-12 text-center" style="margin-top:30px;">
+                <span href="#"><strong>Copyright © 2017&nbsp;優護平台股份有限公司</strong></span><br>
+            </div>
+        </div>
+    </div>
+
+</footer>
